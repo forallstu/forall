fd7d26579e0823dcc85dc24096a80b80bf571078
forallstu <123249861+forallstu@users.noreply.github.com>
2023-01-21 18:50:23 +0200

    Add files via upload

---
+++ without-api-mysql/src/main/java/com/example/demo/Repository/Reppo.java
@@ -0,0 +1,9 @@
+package com.example.demo.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.people;
+
+public interface Reppo extends CrudRepository<people, Integer>{
+
+}
---
+++ without-api-mysql/src/main/java/com/example/demo/WithoutApiMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithoutApiMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithoutApiMysqlApplication.class, args);
+	}
+
+}
